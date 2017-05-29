494f328e0b7102d4d4431a1a572d5f9a1755456f
bugbear <yuhe.wei@upai.com>
2017-05-30 01:28:35 +0800

    add npm deploy; optimize ansible deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,5 @@
+#bin/bash
+SCRIPT_DIR="$(dirname "$0")"
+APP_ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
+cd ${APP_ROOT_DIR}/deploy
+ ansible-playbook -i hosts enter.yml -e 'node=web role=knight' -t core
